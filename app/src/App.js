0f549dcea66f5ab6af66487f2f3aadcb74ff5f94
chudnofskyATD <mpereannor@gmail.com>
2019-10-01 13:45:04 +0000

    App.js class component; fetched data set to state

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
 
-
+    this.state= {
+      mountedApi= [],
+    },
   }
 
+  componentDidMount(){
+    const gitApi = `https://api.github.com/users/mpereannor`
+
+    axios.get(gitApi)
+    .then(res =>{
+      this.setState({mountedApi: res.data})
+        }
+
+    )
+  }
   render(){
     return (
       <div className="App">
